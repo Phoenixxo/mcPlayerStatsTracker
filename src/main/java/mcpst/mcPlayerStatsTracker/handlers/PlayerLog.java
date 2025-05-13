33c003f8e44550abb6f17cc19ca82bfd0e3034e3
Cayden Tisdale <caydentisdale43@gmail.com>
2025-05-13 15:09:42 -0400

    PlayerLog login & logout

--- src/main/java/mcpst/mcPlayerStatsTracker/handlers/PlayerLog.java
+++ src/main/java/mcpst/mcPlayerStatsTracker/handlers/PlayerLog.java
@@ -5,12 +5,14 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 import java.util.HashMap;
 
 public class PlayerLog implements Listener {
 
-    static HashMap<String, Integer> playerLog = new HashMap<>();
+    static HashMap<String, Integer> playerLogins = new HashMap<>();
+    static HashMap<String, Integer> playerLogouts = new HashMap<>();
 
     public PlayerLog(McPlayerStatsTracker plugin) {
         Bukkit.getPluginManager().registerEvents(this, plugin);
@@ -19,11 +21,22 @@ public class PlayerLog implements Listener {
     @EventHandler
     public void onPlayerLogin(PlayerLoginEvent event) {
         String name = event.getPlayer().getName();
-        if (!playerLog.containsKey(name)) {
-            playerLog.put(name, 1);
+        if (!playerLogins.containsKey(name)) {
+            playerLogins.put(name, 1);
         } else {
-            playerLog.merge(name, 1, Integer::sum);
+            playerLogins.merge(name, 1, Integer::sum);
         }
-        Bukkit.getLogger().info(name + ": " + playerLog.get(name) + " login(s).");
+        Bukkit.getLogger().info(name + ": " + playerLogins.get(name) + " login(s).");
+    }
+
+    @EventHandler
+    public void onPlayerLogout(PlayerQuitEvent event) {
+        String name = event.getPlayer().getName();
+        if (!playerLogouts.containsKey(name)) {
+            playerLogouts.put(name, 1);
+        } else {
+            playerLogouts.merge(name, 1, Integer::sum);
+        }
+        Bukkit.getLogger().info(name + ": " + playerLogouts.get(name) + " logout(s).");
     }
 }
