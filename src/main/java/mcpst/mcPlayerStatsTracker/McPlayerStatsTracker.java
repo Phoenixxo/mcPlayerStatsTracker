b6cc271d9d1e039c3e529aae3076e6ea14bf2082
Cayden Tisdale <caydentisdale43@gmail.com>
2025-05-13 15:36:30 -0400

    KD Listener

--- src/main/java/mcpst/mcPlayerStatsTracker/McPlayerStatsTracker.java
+++ src/main/java/mcpst/mcPlayerStatsTracker/McPlayerStatsTracker.java
@@ -1,5 +1,6 @@
 package mcpst.mcPlayerStatsTracker;
 
+import mcpst.mcPlayerStatsTracker.handlers.KDListener;
 import mcpst.mcPlayerStatsTracker.handlers.PlayerLog;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -10,7 +11,9 @@ public final class McPlayerStatsTracker extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         Bukkit.getLogger().info("Player Logger started.");
+        Bukkit.getLogger().info("KD Listener started.");
         new PlayerLog(this);
+        new KDListener(this);
     }
 
     @Override
